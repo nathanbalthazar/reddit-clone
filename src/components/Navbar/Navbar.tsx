f07140ebc358b3e4ac0fa411238aa752d6ce77ab
Nathan Balthazar <nathanbalthazar96@gmail.com>
2022-04-16 13:39:32 -0300

    feat: Navbar Logos and Chakra UI Styling

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,22 @@
-import { Flex } from '@chakra-ui/react';
-import React from 'react';
+import { Flex, Image } from "@chakra-ui/react";
+import React from "react";
 
-const Navbar:React.FC = () => {
-    
-    return (
-         <Flex>
-             
-         </Flex>
-    );
-}
+const Navbar: React.FC = () => {
+  return (
+    <Flex bg="white" height="44px" padding="6px 12px">
+      <Flex align="center">
+        <Image src="/images/redditFace.svg" alt="reddit logo" height="30px" />
+        <Image
+          src="/images/redditText.svg"
+          alt="reddit logo"
+          height="46px"
+          display={{ base: "none", md: "unset" }}
+        />
+      </Flex>
+    </Flex>
+    // <Directory />
+    // <SearchInput />
+    // <RightContent />
+  );
+};
 export default Navbar;
