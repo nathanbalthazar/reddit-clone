d3c0497f2d0728e25207af63f6347070705fd08d
Nathan Balthazar <nathanbalthazar96@gmail.com>
2022-04-18 18:01:05 -0300

    feat: add firebase error handling

--- src/components/Modal/Auth/SignUp.tsx
+++ src/components/Modal/Auth/SignUp.tsx
@@ -4,6 +4,7 @@ import { useSetRecoilState } from "recoil";
 import { authModalState } from "../../../atoms/authModalAtom";
 import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
 import { auth } from "../../../firebase/clientApp";
+import { FIREBASE_ERRORS } from "../../../firebase/errors";
 
 const SignUp: React.FC = () => {
   const setAuthModalState = useSetRecoilState(authModalState);
@@ -102,11 +103,10 @@ const SignUp: React.FC = () => {
           onChange;
         }}
       />
-      {error && (
-        <Text textAlign="center" color="red" fontSize="10pt">
-          {error}
-        </Text>
-      )}
+      <Text textAlign="center" color="red" fontSize="10pt">
+        {error ||
+          FIREBASE_ERRORS[userError?.message as keyof typeof FIREBASE_ERRORS]}
+      </Text>
       <Button
         width="100%"
         height="36px"
