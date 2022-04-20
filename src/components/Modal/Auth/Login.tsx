1c39176e407d3aac7572ada27433aec4404a5eff
Nathan Balthazar <nathanbalthazar96@gmail.com>
2022-04-20 18:26:10 -0300

    feat: add Sign in existing users

--- src/components/Modal/Auth/Login.tsx
+++ src/components/Modal/Auth/Login.tsx
@@ -1,7 +1,10 @@
 import { Button, Flex, Input, Text } from "@chakra-ui/react";
 import React, { useState } from "react";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
 import { useSetRecoilState } from "recoil";
 import { authModalState } from "../../../atoms/authModalAtom";
+import { auth } from "../../../firebase/clientApp";
+import { FIREBASE_ERRORS } from "../../../firebase/errors";
 
 type LoginProps = {};
 
@@ -12,8 +15,14 @@ const Login: React.FC<LoginProps> = () => {
     email: "",
     password: "",
   });
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
 
-  const onSubmit = () => {};
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    signInWithEmailAndPassword(loginForm.email, loginForm.password);
+  };
 
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     // update form state
@@ -65,7 +74,10 @@ const Login: React.FC<LoginProps> = () => {
           onChange;
         }}
       />
-      <Button width="100%" height="36px" mt={2} mb={2} type="submit">
+      <Text align='center' color='red' fontSize='10pt'  >
+        {FIREBASE_ERRORS[error?.message as keyof typeof FIREBASE_ERRORS]}
+      </Text>
+      <Button width="100%" height="36px" mt={2} mb={2} type="submit" isLoading={loading}>
         Log in
       </Button>
       <Flex fontSize="9pt" justifyContent="center">
