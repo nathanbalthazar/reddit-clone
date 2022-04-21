8c30bac61e1488cadc9c9f2ede855c1dad44d0ab
Nathan Balthazar <nathanbalthazar96@gmail.com>
2022-04-21 15:28:00 -0300

    feat: add  User Navbar Icons

--- src/components/Navbar/RightContent/RightContent.tsx
+++ src/components/Navbar/RightContent/RightContent.tsx
@@ -1,12 +1,13 @@
 import { Button, Flex } from "@chakra-ui/react";
-import { signOut } from "firebase/auth";
+import { signOut, User } from "firebase/auth";
 import React from "react";
 import { auth } from "../../../firebase/clientApp";
 import AuthModal from "../../Modal/Auth/AuthModal";
 import AuthButtons from "./AuthButtons";
+import Icons from "./Icons";
 
 type RightContentProps = {
-  user: any;
+  user?: User | null;
 };
 
 const RightContent: React.FC<RightContentProps> = ({ user }) => {
@@ -14,7 +15,8 @@ const RightContent: React.FC<RightContentProps> = ({ user }) => {
     <>
       <AuthModal />
       <Flex justify="center" align="center">
-        {user ? <Button onClick={() => signOut(auth)} >Log out</Button> : <AuthButtons />}
+        {user ? <Icons /> : <AuthButtons />}
+        {/* <Menu /> */}
       </Flex>
     </>
   );
