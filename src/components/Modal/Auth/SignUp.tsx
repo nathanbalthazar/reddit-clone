35adbd13031952b3d32347deff463bfaf0a4532b
Nathan Balthazar <nathanbalthazar96@gmail.com>
2022-05-10 18:33:09 -0300

    add: firebase error handling

--- src/components/Modal/Auth/SignUp.tsx
+++ src/components/Modal/Auth/SignUp.tsx
@@ -2,8 +2,9 @@ import { Input, Button, Flex, Text } from "@chakra-ui/react";
 import React, { useState } from "react";
 import { useSetRecoilState } from "recoil";
 import { authModalState } from "../../../atoms/authModalAtom";
-import { auth } from "../../../firebase/clientApp";
 import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { auth } from "../../../firebase/clientApp";
+import { FIREBASE_ERRORS } from "../../../firebase/errors";
 
 const SignUp: React.FC = () => {
   const setAuthModalState = useSetRecoilState(authModalState);
@@ -105,11 +106,10 @@ const SignUp: React.FC = () => {
         }}
         bg="gray.50"
       />
-      {error && (
-        <Text textAlign="center" color="red" fontSize="10pt">
-          {error}
-        </Text>
-      )}
+      <Text textAlign="center" color="red" fontSize="10pt">
+        {error ||
+          FIREBASE_ERRORS[userError?.message as keyof typeof FIREBASE_ERRORS]}
+      </Text>
       <Button
         width="100%"
         height="36px"
---
+++ src/firebase/errors.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_ERRORS = {
+  "Firebase: Error (auth/email-already-in-use).":
+    "A user with that email already exists",
+  "Firebase: Error (auth/user-not-found).":
+    "A user with that email already exists",
+  "Firebase: Error (auth/wrong-password).":
+    "A user with that email already exists",
+};
