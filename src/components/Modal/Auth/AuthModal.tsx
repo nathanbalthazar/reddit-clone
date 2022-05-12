4ccc750e4ba087f5ca32ca52b8a6f2b70bee6821
Nathan Balthazar <nathanbalthazar96@gmail.com>
2022-05-12 07:59:48 -0300

    add: useAuthState logic to login

--- src/components/Modal/Auth/AuthModal.tsx
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -11,14 +11,17 @@ import {
   Flex,
   Text,
 } from "@chakra-ui/react";
-import React from "react";
+import React, { useEffect } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { useRecoilState } from "recoil";
 import { authModalState } from "../../../atoms/authModalAtom";
+import { auth } from "../../../firebase/clientApp";
 import AuthInputs from "./AuthInputs";
 import OAuthButtons from "./OAuthButtons";
 
 const AuthModal: React.FC = () => {
   const [modalState, setModalState] = useRecoilState(authModalState);
+  const [user, loading, error ] = useAuthState(auth)
 
   const handleClose = () => {
     setModalState((prev) => ({
@@ -26,6 +29,13 @@ const AuthModal: React.FC = () => {
       open: false,
     }));
   };
+
+  useEffect(() => {
+    if (user) handleClose();
+    console.log("user", user);
+    
+  }, [user])
+
   return (
     <>
       <Modal isOpen={modalState.open} onClose={handleClose}>
