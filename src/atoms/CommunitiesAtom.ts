aca962c2333b4e72262d2805d283b5b7f5660cbc
Nathan Balthazar <nathanbalthazar96@gmail.com>
2022-05-18 08:40:31 -0300

    add: atom global community state

--- src/atoms/CommunitiesAtom.ts
+++ src/atoms/CommunitiesAtom.ts
@@ -9,3 +9,23 @@ export interface Community {
   createdAt?: Timestamp;
   imgURL?: string;
 }
+
+interface CommunitySnippet {
+  communityId: string;
+  isModerator?: boolean;
+  imageURL?: string;
+}
+
+interface CommunityState {
+  mySnippets: CommunitySnippet[];
+  // visitedCommunityies
+}
+
+const defaultCommunityState: CommunityState = {
+  mySnippets: [],
+};
+
+export const communityState = atom<CommunityState>({
+  key: "communityState",
+  default: defaultCommunityState,
+});
