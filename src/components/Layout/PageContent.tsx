2e33a625390480e03262ff85d33440dfa4ee2819
Nathan Balthazar <nathanbalthazar96@gmail.com>
2022-05-19 20:32:39 -0300

    add: submit form tabs

--- src/components/Layout/PageContent.tsx
+++ src/components/Layout/PageContent.tsx
@@ -7,9 +7,8 @@ type PageContentProps = {
 
 const PageContent: React.FC<PageContentProps> = ({ children }) => {
   return (
-    <Flex border="1px solid red" justify="center" p="16px 0px">
+    <Flex justify="center" p="16px 0px">
       <Flex
-        border="1px solid green"
         width="95%"
         justify="center"
         maxWidth="860px"
@@ -18,7 +17,6 @@ const PageContent: React.FC<PageContentProps> = ({ children }) => {
           direction="column"
           width={{ base: "100%", md: "65%" }}
           mr={{ base: 0, md: 6 }}
-          border="1px solid blue"
         >
           {children && children[0 as keyof typeof children]}
         </Flex>
@@ -26,7 +24,6 @@ const PageContent: React.FC<PageContentProps> = ({ children }) => {
           direction="column"
           display={{ base: "none", md: "flex" }}
           flexGrow={1}
-          border="1px solid orange"
         >
           {children && children[1 as keyof typeof children]}
         </Flex>
--- src/pages/r/[communityId]/submit.tsx
+++ src/pages/r/[communityId]/submit.tsx
@@ -1,7 +1,22 @@
+import { Box, Text } from '@chakra-ui/react';
 import React from 'react';
+import PageContent from '../../../components/Layout/PageContent';
+import NewPostForm from '../../../components/Posts/NewPostForm';
 
 const submitPostPage:React.FC = () => {
     
-    return <div>Submit Page</div>
+    return (
+        <PageContent>
+            <>
+                <Box p='14px 0px' borderBottom='1px solid' borderColor='white' >
+                    <Text>Create a post</Text>
+                </Box>
+                <NewPostForm />
+            </>
+            <>
+                {/* <About /> */}
+            </>
+        </PageContent>
+    )
 }
 export default submitPostPage;
